c868e83610deebe61c2f225d6ccc9293d855c5fe
David Palma <48089310+david-palma@users.noreply.github.com>
2023-01-26 10:30:31 +0100

    Update ex02.ino

--- ex02/ex02.ino
+++ ex02/ex02.ino
@@ -5,13 +5,13 @@
 // define constants
 const int led_pins[] = { 9, 10, 11, 12, 13 };         // array of LED pins
 const int no_pins    = sizeof(led_pins)/sizeof(int);  // number of pins
-const int between_HL = 500;                           // milliseconds between ON and OFF
+const int between_HL = 200;                           // milliseconds between ON and OFF
 
 // initialisation
 void setup()
 {
     // set the digital pins as outputs using a for loop
-    for(int cnt = 0; cnt < no_pins - 1; cnt++)
+    for(int cnt = 0; cnt < no_pins; cnt++)
     {
         pinMode(led_pins[cnt], OUTPUT);
         digitalWrite(led_pins[cnt], LOW);
@@ -31,7 +31,7 @@ void loop()
     }
 
     // turn the leds ON and OFF from last to first
-    for(int cnt = no_pins - 2; cnt >= 0; cnt--)
+    for(int cnt = no_pins - 2; cnt >= 1; cnt--)
     {
         digitalWrite(led_pins[cnt], HIGH);  // turn the LED on
         delay(between_HL);                  // wait for a second
